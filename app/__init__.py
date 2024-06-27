00969a3840759d1ce062135045a9248f3f2d3aaa
Urvik Lodhe <urviklode007@gmai.com>
2024-06-27 12:22:13 +0530

    comments

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,13 @@ mongo = PyMongo()
 def create_app():
     app = Flask(__name__)
     
+    # Configure the MongoDB connection
     app.config["MONGO_URI"] = "mongodb://localhost:27017/github_events"
+    
+    # Initialize the PyMongo instance with the Flask app
     mongo.init_app(app)
     
+    # Import and register the webhook blueprint
     from app.webhook.routes import webhook
     app.register_blueprint(webhook)
     
--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -7,10 +7,15 @@ webhook = Blueprint('webhook', __name__, url_prefix='/webhook')
 
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
+    """
+    Endpoint to receive and process GitHub webhook events.
+    It handles 'push' and 'pull_request' events and stores them in the MongoDB.
+    """
     data = request.json
     event_type = request.headers.get('X-GitHub-Event')
 
     if event_type == 'push':
+        # Process push event
         event = {
             '_id': f"{data['after']}_PUSH",
             'request_id': data['after'],
@@ -23,9 +28,11 @@ def receiver():
         mongo.db.events.insert_one(event)
 
     elif event_type == 'pull_request':
+        # Process pull request event
         pull_request_action = data['action']
         action = 'PULL_REQUEST'
-        if pull_request_action == 'closed' and data['pull_request']['merged']:
+        # For Merge event
+        if pull_request_action == 'closed' and data['pull_request']['merged']: 
             action = 'MERGE'
         print(action)
         event = {
@@ -43,9 +50,16 @@ def receiver():
 
 @webhook.route('/events', methods=['GET'])
 def get_events():
+    """
+    Endpoint to fetch the latest 10 events from the MongoDB.
+    The events are sorted by timestamp in descending order.
+    """
     events = list(mongo.db.events.find({}, {'_id': 0}).sort('timestamp', -1).limit(10))
     return jsonify(events)
 
 @webhook.route('/', methods=['GET'])
 def index():
+    """
+    Render the index.html template.
+    """
     return render_template('index.html')
